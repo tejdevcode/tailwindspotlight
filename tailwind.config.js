c0bf44f3062bc9c76df8d6d1cbf4a84407d2dc59
tejrdev <tejashkumar.r@uplers.in>
2024-05-23 02:24:14 +0530

    header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
       'h2': ['"Alfa Slab One"', '"Open Sans"'],
       'h3': ['"Bebas Neue"', '"Open Sans"']
     },
+    colors: {
+      'uibrown': '#3d2514',
+      'uired': '#bf2222',
+      'uiyellow': '#ffc045',
+    },
     extend: {},
   },
   plugins: [],
