c915c8f518e7c999169bf8a2eab3fdc174b16d1a
tejrdev <tejashkumar.r@uplers.in>
2024-05-25 05:25:59 +0530

    perelex added

--- src/js/general.js
+++ src/js/general.js
@@ -1,4 +1,8 @@
 
+const currentYear = new Date().getFullYear();
+document.querySelector('#footeryear').innerHTML = currentYear;
+
+new WOW().init();
 
 $(document).ready(function () {
    $('.sliderbox').slick({
@@ -9,5 +13,15 @@ $(document).ready(function () {
       arrows: false,
       adaptiveHeight: true
    });
+
+   $(window).on('scroll', function() {
+    yPos = window.pageYOffset;
+    shift = +315 - yPos * 0.15 + 'px';
+    $('.bookmedia1 ').css('top', shift);
+    shift2 = +1300 - yPos * 0.4 + 'px';
+    $('.bookmedia2 ').css('top', shift2);
+    shift3 = +1300 - yPos * 0.4 + 'px';
+    $('.bookmedia3 ').css('top', shift3);
+  });
 });
 
