4bc47e9f6855d19cd38c853579a912ca08c64175
tejrdev <tejashkumar.r@uplers.in>
2024-05-25 05:54:02 +0530

    perelex added

--- src/js/general.js
+++ src/js/general.js
@@ -11,17 +11,20 @@ $(document).ready(function () {
       speed: 300,
       slidesToShow: 1,
       arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      speed: 1000,
       adaptiveHeight: true
    });
 
-   $(window).on('scroll', function() {
-    yPos = window.pageYOffset;
-    shift = +315 - yPos * 0.15 + 'px';
-    $('.bookmedia1 ').css('top', shift);
-    shift2 = +1300 - yPos * 0.4 + 'px';
-    $('.bookmedia2 ').css('top', shift2);
-    shift3 = +1300 - yPos * 0.4 + 'px';
-    $('.bookmedia3 ').css('top', shift3);
-  });
+   $(window).on('scroll', function () {
+      yPos = window.pageYOffset;
+      shift = +315 - yPos * 0.15 + 'px';
+      $('.bookmedia1 ').css('top', shift);
+      shift2 = +1300 - yPos * 0.4 + 'px';
+      $('.bookmedia2 ').css('top', shift2);
+      shift3 = +1300 - yPos * 0.4 + 'px';
+      $('.bookmedia3 ').css('top', shift3);
+   });
 });
 
