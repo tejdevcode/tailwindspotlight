6313340fdf5469505966fdb8fb0858f7802d2e56
tejrdev <tejashkumar.r@uplers.in>
2024-05-26 00:23:37 +0530

    lp complt

--- src/js/general.js
+++ src/js/general.js
@@ -11,20 +11,35 @@ $(document).ready(function () {
       speed: 300,
       slidesToShow: 1,
       arrows: false,
-      autoplay: true,
+      autoplay: false,
       autoplaySpeed: 2500,
       speed: 1000,
       adaptiveHeight: true
    });
 
    $(window).on('scroll', function () {
-      yPos = window.pageYOffset;
-      shift = +315 - yPos * 0.15 + 'px';
-      $('.bookmedia1 ').css('top', shift);
-      shift2 = +1300 - yPos * 0.4 + 'px';
-      $('.bookmedia2 ').css('top', shift2);
-      shift3 = +1300 - yPos * 0.4 + 'px';
-      $('.bookmedia3 ').css('top', shift3);
+      yPos = window.scrollY;
+      if (window.innerWidth > 767) {
+         shift = +315 - yPos * 0.15 + 'px';
+         $('.bookmedia1 ').css('top', shift);
+         shift2 = +1300 - yPos * 0.4 + 'px';
+         $('.bookmedia2 ').css('top', shift2);
+         shift3 = +500 - yPos * 0.4 + 'px';
+         $('.bookmedia3 ').css('top', shift3);
+      }
    });
+
+   /* nav */
+   $('.navbtn').click(function () {
+      if ($('.navbox').hasClass('hidden')) {
+         $(this).addClass('on');
+         $('.navbox').removeClass('hidden');
+      } else {
+         $(this).removeClass('on');
+         $('.navbox').addClass('hidden');
+      }
+
+   });
+
 });
 
